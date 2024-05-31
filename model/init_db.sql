291eec2060659c9d2746a1c42143388931647d5d
Shannon Palumbo <sspalumbo@gmail.com>
2024-05-31 16:19:58 +0200

    added db

--- model/init_db.sql
+++ model/init_db.sql
@@ -1,34 +1,32 @@
---
--- Drop Tables
---
+DROP TABLE IF EXISTS resources;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS users;
 
-SET foreign_key_checks = 0;
-DROP TABLE if exists steps;
-SET foreign_key_checks = 1;
 
---
--- Create Tables
---
-
-CREATE TABLE `steps`(
-    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `Description` VARCHAR(255) NOT NULL,
-    `Next_1` BIGINT NOT NULL,
-    `Next_2` BIGINT NOT NULL,
-    `Next_3` BIGINT NOT NULL,
-    `Text_1` VARCHAR(255) NOT NULL,
-    `Text_2` VARCHAR(255) NOT NULL,
-    `Text_3` VARCHAR(255) NOT NULL
+CREATE TABLE `resources`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `link_url` VARCHAR(255) NOT NULL,
+    `vid_url` VARCHAR(255) NOT NULL,
+    `doc` VARCHAR(255) NOT NULL,
+    `img` VARCHAR(255) NOT NULL,
+    `notes` LONGTEXT NOT NULL,
+    `category_id` INT UNSIGNED NOT NULL,
+    `user_id` INT UNSIGNED NOT NULL
 );
-
-INSERT INTO `steps` (id, Description, Next_1, Next_2, Next_3, Text_1, Text_2, Text_3) VALUES
-(1, 'You have chosen well. You will be painting STEEL!', 2, 0, 0, 'proceed to step 2...', '', ''),
-(2, 'Using an orbital sander, sand the entire surface with 120-150 grit sandpaper until smooth and clean of rust and debris.', 3, 0, 0, 'on to step 3!', '', ''),
-(3, 'Clean and degrease THOROUGHLY with Denatured Alcohol.', 4, 0, 0, 'go to step 4', '', ''),
-(4, 'What do you need to do with this steel?', 5, 6, 7, 'It just needs to go FLAT BLACK, no body work.', 'It needs Matthews Paint but NO body work.', 'It needs body work, or it has been previously painted.'),
-(5, 'Paint with Blacksmith brand DTM', 0, 0, 0, 'All done!', '', ''),
-(6, 'Prime with Etching Primer', 10, 9, 0, 'Quality Checkpoint! Needs additional repair? Go to step 10.', 'Passes Quality Check? Go to step 9!', ''),
-(7, 'Do necessary body work', 8, 0, 0, 'Move to Step 8', '', ''),
-(8, 'Prime with Epoxy Primer', 9, 0, 0, 'Move to Step 9', '', ''),
-(9, 'Paint with Matthews.', 0, 0, 0, 'All done, good job!', '', ''),
-(10, 'Make any needed repairs and spot prime with lacquer or epoxy primer.', 9, 0, 0, 'go to step 9.', '', '');
+CREATE TABLE `categories`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `type` VARCHAR(255) NOT NULL,
+    `user_id` INT UNSIGNED NOT NULL
+);
+CREATE TABLE `users`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    `resources` ADD CONSTRAINT `resources_category_id_foreign` FOREIGN KEY(`category_id`) REFERENCES `categories`(`id`);
+ALTER TABLE
+    `resources` ADD CONSTRAINT `resources_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `users`(`id`);
+ALTER TABLE
+    `categories` ADD CONSTRAINT `categories_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `users`(`id`);
