2e51a8b4f896a7ef86bc180a11585ed931695c2a
Silke <silke.kast@gmail.com>
2024-06-04 14:47:31 +0200

    adjusted backend routes and created upload form

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(cors()); // add after 'app' is created
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
--- client/src/pages/upload.jsx
+++ client/src/pages/upload.jsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+
 
 export default function upload() {
+const [categories, setCategories] = useState([]);
+const [newCategory, setNewCategory] = useState({
+    type:"",
+    user_id:""
+});
+const [resources, setResources] = useState([]);
+const [input, setInput] = useState({
+    link_url:"",
+    vid_url:"",
+    doc:"",
+    img:"",
+    notes:"",
+    category_id:"",
+    user_id:""
+});
+
+useEffect(() => {
+    //fetch categories(
+    fetch("api/categories")
+    .then((res) => res.json())
+    .then((data) => {
+        setCategories(data);
+    });
+},[]);
+
   return (
-    <div>upload</div>
+    <div>
+        <form action="upload">
+            <label htmlFor="link_url">Website Link</label>
+            <input type="text" name="link_url" />
+            <label htmlFor="vid_url">Video Link</label>
+            <input type="text" name="vid_url"  />
+            <label htmlFor="doc">Select Document</label>
+            <input type="file" name="doc" />
+            <label htmlFor="img">Select Image</label>
+            <input type="file" name="img" />
+            <label htmlFor="notes">Notes</label>
+            <textarea name="notes"></textarea>
+            <label htmlFor="category">Category</label>
+            <select name="category_id" id="">
+                <option value="">Select Category</option>            
+            {categories.map(category => (
+                <option key={category.id} value={category.id}>{category.type}</option>) 
+            )} 
+            </select>               
+            <button>Upload</button>
+        </form>
+        <br />
+        <form action="submit">
+            <label htmlFor="category">New Category</label>
+            <input type="text" value={newCategory.type} />
+            <button>Add Category</button>
+        </form>
+    </div>
   )
 }
