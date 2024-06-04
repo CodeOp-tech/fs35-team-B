456d99c268718c0bed90546f59a9f37703986fb7
Shannon Palumbo <sspalumbo@gmail.com>
2024-06-04 15:05:19 +0200

    pulled

--- client/src/components/sideNav.jsx
+++ client/src/components/sideNav.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-export default function sideNav() {
+ function sideNav() {
+    
   return (
-    <div>sideNav</div>
+   <div> 
+   
+  </div>
   )
 }
+
+export default sideNav;
