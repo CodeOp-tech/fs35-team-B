af097a01bf07911fe0fe50db5c801a226584b769
AlyssaMaeBaruc <159028804+AlyssaMaeBaruc@users.noreply.github.com>
2024-06-05 17:53:35 +0200

    tringg

--- client/src/components/requireAuth.jsx
+++ client/src/components/requireAuth.jsx
@@ -2,8 +2,6 @@ import { useContext } from "react";
 import authContext from "../contexts/authContext";
 import { Navigate } from "react-router-dom";
 
-//if things are ok, I let you through 
-//if they're not, I kick you out to the login page
 
 export default function RequireAuth({ children }) {
   const { isLoggedIn } = useContext(authContext);
