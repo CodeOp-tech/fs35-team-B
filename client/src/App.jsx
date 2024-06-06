8b3cc8f8153d525116a15e80e8519f5f5d27cac3
Shannon Palumbo <sspalumbo@gmail.com>
2024-06-06 13:58:23 +0200

    completed registration and login functionsgit add .!

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,8 @@ import './App.css';
 import {Routes, Route, Link, useNavigate} from "react-router-dom";
 import Home from "./pages/home";
 import Dashboard from "./pages/dashboard";
-import Login from "./pages/login";
-import SignUp from "./pages/signUp";
+import Login from "./pages/login";// deleting this page!!
+import Register from "./pages/register";// client\src\pages\register.jsx
 import Upload from "./pages/upload";
 import Resources from "./pages/resources";
 import authContext from "./contexts/authContext";
@@ -18,25 +18,30 @@ import TopNav from "./components/TopNav";
 function App() {
 
   const navigate = useNavigate();
-
   const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
+  const [username, setUserName] = useState(localStorage.getItem("username") || "");
 
   useEffect(() => {
     console.log('Login Status Changed', isLoggedIn);
   }, [isLoggedIn]);
 
-  function signIn () {
+  function signIn (username) { //added "context" items here, trial run
     setIsLoggedIn(true);
-    navigate("/"); // Maybe this needs to change since we want to change the state of the home page but not LEAVE it??
+    setUserName(username);
+    localStorage.setItem("username", username)
+    navigate("/"); 
   }
 
-  function signOut () {
+  function signOut() {
     setIsLoggedIn(false);
+    setUserName("");
+    localStorage.removeItem("username");
     navigate("/"); //Again, whats the best way to handle this? 
   }
 
   const auth = {
     isLoggedIn,
+    username,
     signIn,
     signOut
   }
@@ -51,11 +56,13 @@ function App() {
   <Link to="/upload">Upload</Link>
   <Link to="/">Home</Link>
   <Link to="/resources/:id">Resources</Link>
+  <Link to="/register">Register</Link>
 </div>
 <div>
   <Routes>  
-    <Route path="/signUp" element={<SignUp />}/>
+    
     <Route path="/" element={<Home />}/>
+    <Route path="/register" element={<Register />}/>
     <Route path="/resources/:id" element={<Resources />}/>
     <Route path="/upload" element={<Upload />}/>
     <Route path="/dashboard" element={<Dashboard />}/>
--- client/src/contexts/authContext.js
+++ client/src/contexts/authContext.js
@@ -1,5 +1,10 @@
 import { createContext } from "react"; 
 
-const authContext = createContext();
+const authContext = createContext({
+    isLoggedIn: false, //from context, trial run
+    username: null,
+    signIn: () => {},
+    signOut: () => {}
+});
 
 export default authContext;
