7e3f9a0792dcebaf415bc635c0cf5a67a018ba4a
AlyssaMaeBaruc <159028804+AlyssaMaeBaruc@users.noreply.github.com>
2024-06-10 17:34:43 +0200

    filtered resources based on user id and added token to the front end

--- client/src/pages/resources.jsx
+++ client/src/pages/resources.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import {useParams, useSearchParams} from "react-router-dom";
 
-
 function resources() {
  
-    // what i want to do here is to fetch the resources route 
-    // put out the list of resources, i need 
-    //  in the front end, we always pass the token along 
+    // what i want to do here is to fetch the resources route
+    // put out the list of resources, i need
+    //  in the front end, we always pass the token along
     // http://localhost:5173/resources?category_id=3
 
-// state to store fetched resources 
+
+// state to store fetched resources
+
 
 const [resourcesData, setResourcesData ] = useState ([]);
 
@@ -19,15 +20,25 @@ const [searchParams] = useSearchParams();
 
 // use effect when fetching resources
 
+
 useEffect(() => {
     console.log(searchParams.get("category_id"))
+
 
     const fetchResources = async () => {
       try {
         //get the value of 'category_id from search params
         const category = searchParams.get('category_id');
-        
-        const response = await fetch(`/api/resources?category=${category}`);
+        const token = localStorage.getItem('token');
+
+        const headers = {};
+        if (token) {
+          headers['Authorization'] = `Bearer ${token}`;
+        }
+       
+        const response = await fetch(`/api/resources?category=${category}`, {
+          headers
+        });
         if (!response.ok) {
           throw new Error('error found');
         }
@@ -38,11 +49,14 @@ useEffect(() => {
       }
     };
 
+
 // fetch data when search params changes
 if (searchParams) {
     fetchResources();
   }
 }, [searchParams]);
+
+
 
 
 return (
@@ -66,4 +80,7 @@ return (
 );
 };
 export default resources;
+
+
+
 
