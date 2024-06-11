b1fc6b5b38743022ab6cf1da8e08db4215c29b4f
Shannon Palumbo <sspalumbo@gmail.com>
2024-06-11 13:36:23 +0200

    more styling and breakpoint fixes

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header-content">
+                <h1 className="site-title">The Confused Coder 🦆</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
